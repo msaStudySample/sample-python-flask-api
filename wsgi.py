b6eff0035e8d77eaafe7b347218457277bedcec5
서명석(Myeongseok Seo) 과장 낙스넷 <myeongseok1.seo@doosan.com>
2020-10-14 13:36:05 +0900

    config secert key 수정

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, text
 from extentions import db
 import os
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 
 # bp = Blueprint('idx', __name__, url_prefix='/')
 
@@ -11,22 +12,24 @@ def create_app():
     application = Flask(__name__)
     if os.environ.get('APP_ENV') == 'prod':
         print('prod')
-        application.config.from_object('config')
+        application.config.from_object('config_prod')
     elif os.environ.get('APP_ENV') == 'dev':
         print('dev')
         application.config.from_object('config_dev')
     else:
         print('local')
         application.config.from_object('config_local')
+        print("SECRET_KEY : ", application.config.get("SECRET_KEY"))
 
     application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    application.secret_key = 'samplewithflask'
+    # application.secret_key = 'samplewithflask'
 
     CORS(application, supports_credentials=True)
     configure_extensions(application)
 
     database = create_engine(application.config['DB_URL'], encoding='utf-8')
     application.database = database
+
 
     CORS(application, supports_credentials=True)
 
@@ -62,6 +65,12 @@ app.add_url_rule('/', endpoint='API_v1')
 app.register_blueprint(jwt_api.jwt_api_v1)
 app.add_url_rule('/', endpoint='JWT_API_v1')
 
+# app.config['SECRET_KEY'] = 'I love you!'
+# app.config['BCRYPT_LEVEL'] = 10
+print("SECRET_KEY : ", app.config.get("SECRET_KEY"))
+print("BCRYPT_LEVEL : ", app.config.get("BCRYPT_LEVEL"))
+# TODO create_app() 안에서 encryption이 좋을지 확인
+bcrypt = Bcrypt(app)
 
 if __name__ == '__main__':
     app.run(port=8080)
